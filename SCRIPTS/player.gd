be0471b4e32a9fedfcdafbb3a0f0fe1781971d18
luxkatsu <lucasassis.design@gmail.com>
2024-04-06 17:45:19 -0300

    world environment added and more
    hand node, flashlight etc

--- SCRIPTS/player.gd
+++ SCRIPTS/player.gd
@@ -9,6 +9,8 @@ extends CharacterBody3D
 
 @onready var head = $Head
 @onready var camera = $Head/Camera3D
+@onready var hand = $Hand
+@onready var flashlight = $Hand/SpotLight3D
 
 var direction = Vector3.ZERO
 var head_y_axis = 0.0
@@ -21,16 +23,20 @@ func _input(event):
 	if event is InputEventMouseMotion:
 		head_y_axis += event.relative.x * cameraSensitivity
 		camera_x_axis += event.relative.y * cameraSensitivity
+		camera_x_axis = clamp(camera_x_axis, -90.0, 90)
 
 	if Input.is_key_pressed(KEY_ESCAPE):
 		get_tree().quit()
 
 func _process(delta):
-	direction = Input.get_axis("left", "right") * Vector3.RIGHT + Input.get_axis("backward", "forward") * Vector3.FORWARD
+	direction = Input.get_axis("left", "right") * head.basis.x + Input.get_axis("forward", "backward") * head.basis.z
 	velocity = velocity.lerp(direction * playerSpeed + velocity.y * Vector3.UP, playerAcceleration * delta)
 	
 	head.rotation.y = lerp(head.rotation.y, -deg_to_rad(head_y_axis), cameraAcceleration * delta)
 	camera.rotation.x = lerp(camera.rotation.x, -deg_to_rad(camera_x_axis), cameraAcceleration * delta)
+	
+	hand.rotation.y = -deg_to_rad(head_y_axis)
+	flashlight.rotation = -deg_to_rad(camera_x_axis)
 	
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y += jumpForce
