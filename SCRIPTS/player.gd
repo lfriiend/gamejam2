518354c06a6a7e0163003961ee233cde87bf24ad
Lucas Sales <103463820+lfriiend@users.noreply.github.com>
2024-04-06 03:46:12 -0300

    commit

--- SCRIPTS/player.gd
+++ SCRIPTS/player.gd
@@ -1,9 +1,11 @@
 extends CharacterBody3D
 
-onready var head = $Head
-onready var head_x = $Head/HeadXRotation
+@onready var head = $Head
+@onready var head_x = $Head/HeadXRotation
 
 var sensitivity = -0.1
+
+const SPEED = 2.0
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -18,6 +20,15 @@ func _process(delta):
 	var head_basis = head.get_global_transform().basis
 	var direction = Vector3.ZERO
 
-if Input.is_action_pressed("up"):
-	direction -= head_basis.z
-	
+	if Input.is_action_just_pressed("up"):
+		direction -= head.basis.z
+	elif Input.is_action_just_pressed("down"):
+		direction += head_basis.z
+	if Input.is_action_just_pressed("left"):
+		direction -= head.basis.x
+	elif Input.is_action_just_pressed("right"):
+		direction += head_basis.x
+		
+	direction = direction.normalized()
+	var direspeed = direction * SPEED
+	move_and_slide(direspeed)
